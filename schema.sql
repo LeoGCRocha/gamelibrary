b2edf12a3cdfc6d51cc01e028663ab8a3ba7b421
Leonardo Gideão Costa Rocha <gideonveloz@gmail.com>
2018-10-01 08:39:23 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,13 @@
-
+create type classind as enum ('l','10','12','14','16','18')
+create table user(
+    nome varchar(500) not null,
+    imagemDir text default 'no-game-img.jpg',
+    preco numeric(5,2) not null,
+    lancamento int not null check (lancamento>=1970),
+    class_ind classind not null,
+    windows boolean default true,
+    linux boolean default true,
+    mac boolean default true,
+    desenvolvedora varchar(500) not null,
+    distribuidor varchar(500) not null,
+    );
