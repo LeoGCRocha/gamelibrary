e870d056ac4920704923ab01e13d03b07a930a90
Veigaribo <gabriellopvei@live.com>
2018-10-01 19:11:12 -0300

    Trabalhadinha no sidenav

--- application/views/index.php
+++ application/views/index.php
@@ -1,7 +1,7 @@
 <?php $this->load->helper('url'); ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -34,16 +34,26 @@
 	</nav>
 
 	<ul class="sidenav" id="mobile-demo">
-		<li><a href="sass.html">Sass</a></li>
-		<li><a href="badges.html">Components</a></li>
-		<li><a href="collapsible.html">Javascript</a></li>
-		<li><a href="mobile.html">Mobile</a></li>
+		<li class="wrapper">
+			<img src="<?php echo base_url();?>/static/res/logo2.png" id="sidenavlogo" height="200">
+			<h2 id="sidenavhtext">Game Library</h2>
+		</li>
+		<li>
+			<hr><br>
+		</li>
+		<li>
+			<a href="<?php echo site_url('controller/login_form'); ?>"><i class="material-icons left purple-text text-darken-4">person</i>Login</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('controller/reg_form'); ?>"><i class="material-icons left purple-text text-darken-4">person_add</i>Cadastro</a>
+		</li>
 	</ul>
 
 	<img id="backimg" src="<?php echo base_url();?>/static/res/back.jpg">
 	<span id="backmask"></span>	
 
-	<div class="section">
+	<div class="section wrapper" id="mainsection">
+		<h1>Só a ponta do teste</h1>
 	</div>
 
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
